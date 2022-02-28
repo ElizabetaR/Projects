d9a4c25fdfc2ff00ba000760160736fb1362993a
ElizabetaR <94299917+ElizabetaR@users.noreply.github.com>
2022-02-28 20:30:45 +0100

    Add files via upload

---
+++ tesla clone/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Home />
+    </div>
+
+
+  );
+};
+
+export default App;
